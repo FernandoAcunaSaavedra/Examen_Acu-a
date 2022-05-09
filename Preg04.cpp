08c36360d0a7ef7dccc259bf3254af517f530dde
FernandoAcunaSaavedra <fernandoacunasaavedra@gmail.com>
2022-05-09 18:10:06 -0500

    desarrolladores registrados Fernando Acuña Saavedra

---
+++ Preg04.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	int num;
+	int div=0;//haz un acumulativo de cuantos suman entre si los divisores del numero
+	/*int menors;*/
+	cout<<"Introduzca un numero entero y le diremos si es perfecto o no."<<endl;
+	cout<<"Numero: ";
+	cin>>num;
+	for (int i=1; i<num; i++){
+		if (num%i==0)
+			div +=i;
+	}
+	if (num==div)
+	{
+		cout<<num<< " es perfecto";
+	}
+	else
+	{
+		cout<<num<<" no es perfecto";
+	}
+	return 0;
+}
